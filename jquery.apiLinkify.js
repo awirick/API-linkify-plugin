c4749673e6a0ed043923dd541f80bc5456faa490
Andrew Wirick <awirick@andrew-wiricks-macbook-pro.local>
2010-07-14 15:16:48 -0500

    initial checkin of plugin

---
+++ jquery.apiLinkify.js
@@ -0,0 +1,17 @@
+;(function ($, undefined) {
+  $.fn.apiLinkify = function () {
+    var jQueryMethods = /^[\.\$(jQuery)][\w\.]+/,
+        jQueryApiSite = "http://api.jquery.com/";
+    
+    this.each(function (idx, val) {
+      var match = val.innerHTML.match(jQueryMethods), url;
+      
+      if (match) {
+        url = match[0]
+          .replace(/^\./,'')
+          .replace(/^\$/,'jQuery');
+        $(val).wrapInner('<a href="' + jQueryApiSite + url + '"></a>');
+      }
+    });
+  };
+})(jQuery);
